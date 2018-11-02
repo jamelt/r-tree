e57a876ee0822cc68983888034df61cbe1b3f333
Jamel Toms <jameltoms@gmail.com>
2018-11-02 02:07:45 -0400

    Simplified path

---
+++ src/path.ts
@@ -0,0 +1,40 @@
+import { Entry } from './entry';
+import { Node } from './node';
+import { mixinDeep } from './utils';
+
+export type Element = Node | Entry;
+
+export interface Path {
+  push(element: Element): Path;
+  pop(): Element;
+  value(index?: number): Element | undefined;
+  length(): number;
+}
+
+export function pathCreate(): Path {
+  let sequence: Element[] = [];
+  let currentIndex = -1;
+
+  const instance = {};
+
+  function push(element: Element): Path {
+    // TODO Validate node -> entry -> node -> entry ...
+    sequence.push(element);
+    currentIndex += 1;
+    return <Path>instance;
+  }
+
+  function pop(): Element | undefined {
+    currentIndex -= 1;
+    return sequence.pop();
+  }
+
+  function value(index?: number): Element | undefined {
+    if (index === undefined) return sequence[currentIndex];
+    return sequence[index];
+  }
+
+  const length = () => sequence.length;
+
+  return Object.freeze(mixinDeep(instance, { push, pop, value, length }));
+}
