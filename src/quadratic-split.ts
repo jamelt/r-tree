9387cddb6376fe6e8dffa7164424a16d8422b1b1
Jamel Toms <jameltoms@gmail.com>
2018-11-04 15:50:10 -0500

    Started working on quadrtic split

--- src/quadratic-split.ts
+++ src/quadratic-split.ts
@@ -1,5 +1,23 @@
-import { SplitAlgorithm } from './split';
+import { Entry } from './entry';
+import { Seeds, Split, SplitAlgorithm, SplitAssignment } from './split';
 
 export function quadraticSplitAlgorithm(): SplitAlgorithm {
-  return undefined;
+
+  function pickNext(remaining: Entry[], split: Split): SplitAssignment {
+    return undefined;
+  }
+
+  function pickSeeds(remaining: Entry[]): Seeds {
+    seeds = createSeeds
+    remaining.forEach((x, i) => {
+      remaining.slice(i+1).forEach(y => {
+
+      });
+    });
+    return undefined;
+  }
+
+  return {
+    pickNext, pickSeeds
+  };
 }
