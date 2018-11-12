88395242d8d813adb3c354e05b627b1f63c1b4ba
Jamel Toms <jameltoms@gmail.com>
2018-11-12 00:04:01 -0500

    Fixed tests but Split is not working appropriately.

--- src/parent.ts
+++ src/parent.ts
@@ -1,5 +1,5 @@
-import { Entry } from './entry';
-import { Node } from './node';
+import { Entry, NULL_ENTRY } from './entry';
+import { Node, NULL_NODE } from './node';
 import { Path } from './path';
 
 export interface Parent {
@@ -8,7 +8,8 @@ export interface Parent {
 }
 
 export function loadParentFn(path: Path) {
-  return function (): Parent {
-    return { entry: <Entry>path.pop(), node: <Node>path.pop() };
+  return function(): Parent {
+    if (path.length() < 2) return { entry: NULL_ENTRY, node: NULL_NODE };
+    else return { entry: <Entry>path.pop(), node: <Node>path.pop() };
   };
 }
