cb4c4454f17ba9d3b8ef4e846a8aac0663973bab
Jamel Toms <jameltoms@gmail.com>
2019-03-29 02:11:49 -0400

    Fixes #10 - Added tie breaker code to split entry assignment

--- src/quadratic-split.ts
+++ src/quadratic-split.ts
@@ -1,14 +1,7 @@
 import { Entry } from './entry';
-import { nodeRegion } from './node';
+import { nodeRegion, Node } from './node';
 import { regionArea, regionEnlarge } from './region';
-import {
-  Seeds,
-  seedsCreate,
-  Split,
-  SplitAlgorithm,
-  SplitAssignment,
-  splitAssignmentCreate
-} from './split';
+import { Seeds, seedsCreate, Split, SplitAlgorithm, SplitAssignment, splitAssignmentCreate } from './split';
 
 export function quadraticSplitAlgorithm(): SplitAlgorithm {
   function pickSeeds(remaining: Entry[]): Seeds {
@@ -34,9 +27,10 @@ export function quadraticSplitAlgorithm(): SplitAlgorithm {
   function pickNext(remaining: Entry[], split: Split): SplitAssignment {
     let assignment = splitAssignmentCreate();
     let growth = Number.NEGATIVE_INFINITY;
+    let nodes = split.toArray();
 
     remaining.forEach(entry => {
-      split.toArray().forEach(node => {
+      nodes.forEach(node => {
         const region = nodeRegion(node);
         const combined = regionEnlarge(region, entry.region);
         const diff = regionArea(combined) - regionArea(region);
@@ -45,11 +39,23 @@ export function quadraticSplitAlgorithm(): SplitAlgorithm {
           assignment.entry = entry;
           assignment.node = node;
           growth = diff;
+        } else if (diff === growth) {
+          assignment.node = tieBreaker(split);
         }
       });
     });
 
     return assignment;
+  }
+
+  function tieBreaker({ left, right }: Split): Node {
+    const leftArea = regionArea(nodeRegion(left));
+    const rightArea = regionArea(nodeRegion(right));
+    if (leftArea === rightArea) {
+      return left.entries.length < right.entries.length ? left : right;
+    } else {
+      return leftArea < rightArea ? left : right;
+    }
   }
 
   return {
--- src/test/insert.test.ts
+++ src/test/insert.test.ts
@@ -16,13 +16,13 @@ test('insert', () => {
 test('insert-stress', () => {
   const rtree = RTree({ minEntries: 1, maxEntries: 4 });
   const debug = createDebugFn(() => rtree.inspect(), '*');
-  const count = 50;
-  const space = 100;
-  const debugCount = 5;
+  const count = 15;
+  const range = 100;
+  const debugCount = 15;
 
   let entry;
   for (let i = 0; i < count; i++) {
-    entry = generateDataEntry(2, space);
+    entry = generateDataEntry(2, range);
     rtree.insert(entry.id, entry.region);
     if (i % (count / debugCount) === 0) debug();
   }
